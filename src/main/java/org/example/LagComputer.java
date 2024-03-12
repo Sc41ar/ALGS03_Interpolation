5cd99e59b80a902142bd5a76b1bcb6308984c8b5
Nikita <mrniki2396@gmail.com>
2024-03-13 01:53:33 +0700

    Вывод полинома в консоль

--- src/main/java/org/example/LagComputer.java
+++ src/main/java/org/example/LagComputer.java
@@ -14,14 +14,50 @@ public class LagComputer {
         double sum = 0;
         for (int i = 0; i < args.length; i++) {
             double numerator = 1, denominator = 1;
-            for (int j = 0; j < args.length; j++) {
+            for (int j = 0; j < args.length; j++) { // перебираем все аргументы, кроме текущего
                 if (j != i) {
-                    numerator *= (x - args[j]);
+                    numerator *= (x - args[j]); // по формуле
                     denominator *= (args[i] - args[j]);
                 }
             }
-            sum += (numerator / denominator) * values[i];
+            sum += (numerator / denominator) * values[i]; // добавляем к сумме значение интерполяционного многочлена в точке x
         }
-        return sum;
+        return sum; // возвращаем значение интерполяционного многочлена в точке x
     }
+
+
+    public String getInterpolationPolynomial() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            double coefficient = 1;
+            //вычисляем числитель
+            for (int j = 0; j < args.length; j++) {
+                if (j != i) {
+                    coefficient *= (args[i] - args[j]);
+                }
+            }
+            //делим значение функции на числитель
+            coefficient = values[i] / coefficient;
+            String term = String.format("%.2f", coefficient);
+            //если первое слогаемое - не добавляем плюс
+            if (i == 0) {
+                sb.append(term);
+            } else {
+                sb.append(" + ");
+                if (Math.abs(coefficient) != 1) {
+                    sb.append(term);
+                }
+            }
+            //добавляем к строке неизвестное
+            for (int j = 0; j < args.length; j++) {
+                if (j != i) {
+                    sb.append(" * (x - ");
+                    sb.append(String.valueOf(args[j]));
+                    sb.append(")");
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 }
